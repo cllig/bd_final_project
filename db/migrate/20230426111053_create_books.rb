04e26de11e453614363e95a655a4391e6bc55dc7
claire lignon <claire.lignon@gmail.com>
2023-04-26 13:14:00 +0200

    add user book and order models

---
+++ db/migrate/20230426111053_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :category
+      t.float :price
+      t.string :description
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
