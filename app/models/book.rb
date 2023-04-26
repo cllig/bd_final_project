ff4c2de025ea61a9ed9a81598c5f34629af6d1db
claire lignon <claire.lignon@gmail.com>
2023-04-26 13:19:51 +0200

    add validates to models and create little seed for test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,17 @@
 class Book < ApplicationRecord
+  CATEGORY = %w[
+    Action/Aventure
+    Histoire/Bio
+    Comédie
+    Drame
+    Fantastique
+    Horreur
+    Police/Thriller
+  ]
+
   belongs_to :user
+
+  validates :price, presence: true
+  validates :category, presence: true
+  validates :name, presence: true
 end
