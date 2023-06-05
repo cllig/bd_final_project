a8c37dbc1e8bab1116318ca253d024a7c053baa2
claire lignon <claire.lignon@gmail.com>
2023-06-05 11:16:53 +0200

    float validation & comma

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ApplicationRecord
   has_many_attached :photos, dependent: :destroy
 
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 1, separator: ","}
   validates :category, presence: true
 
   include PgSearch::Model
